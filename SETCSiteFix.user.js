aea707bc9a923c63a614e6b6202bdb82d15d7366
Sundar Raman <ssrmit62@gmail.com>
2017-06-01 10:26:50 +0530

    Fixing the issue in chrome which doesnt support String#contains instead using indexOf

--- SETCSiteFix.user.js
+++ SETCSiteFix.user.js
@@ -14,12 +14,15 @@ main();
 function main() {
 	var images = document.getElementsByTagName('img');
 	for (var img of images) {
-		if(img.src.contains('CMimg')) {
+		if(~img.src.indexOf('CMimg')) {
 			img.width = '200'; 
 		}
-		if(img.src.contains('jaya') || img.src.contains('CMimg')) {
+		if(~img.src.indexOf('jaya') || ~img.src.indexOf('CMimg')) {
 			img.src = 'https://upload.wikimedia.org/wikipedia/commons/8/81/TamilNadu_Logo.svg';
+			console.log('image replaced');
 		}
 	}
-	$('td[class=topwelcome]').remove();
+	if ($) {
+		$('td[class=topwelcome]').remove();
+	}
 }
